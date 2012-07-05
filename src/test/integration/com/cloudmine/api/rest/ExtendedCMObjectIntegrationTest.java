b1b7f00a6bbda8df755fbb9db79d1c0f859780ed
JohnMcCarthy <john@cloudmine.me>
2012-07-05 16:11:57 -0400

    CMObject serialization and deserialization testing

---
+++ src/test/integration/com/cloudmine/api/rest/ExtendedCMObjectIntegrationTest.java
@@ -0,0 +1,28 @@
+package com.cloudmine.api.rest;
+
+import com.cloudmine.test.CloudMineTestRunner;
+import com.cloudmine.test.ExtendedCMObject;
+import com.cloudmine.test.ServiceTestBase;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static com.cloudmine.test.AsyncTestResultsCoordinator.waitThenAssertTestResults;
+
+/**
+ * <br>
+ * Copyright CloudMine LLC. All rights reserved<br>
+ * See LICENSE file included with SDK for details.
+ */
+@RunWith(CloudMineTestRunner.class)
+public class ExtendedCMObjectIntegrationTest extends ServiceTestBase{
+
+    @Test
+    public void testSave() {
+        ExtendedCMObject object = new ExtendedCMObject();
+        object.save(hasSuccessAndHasModified(object));
+        waitThenAssertTestResults();
+
+        service.asyncLoadObject(object.getObjectId(), hasSuccessAndHasLoaded(object));
+        waitThenAssertTestResults();
+    }
+}
