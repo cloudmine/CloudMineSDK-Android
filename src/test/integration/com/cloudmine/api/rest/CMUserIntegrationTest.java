b0b8a344dc40a2ee7bcfd88122a46279b5036d5d
JohnMcCarthy <john@cloudmine.me>
2012-07-11 16:01:36 -0400

    Testing of setting user object ids after creation

--- src/test/integration/com/cloudmine/api/rest/CMUserIntegrationTest.java
+++ src/test/integration/com/cloudmine/api/rest/CMUserIntegrationTest.java
@@ -2,14 +2,17 @@ package com.cloudmine.api.rest;
 
 import com.cloudmine.api.CMSessionToken;
 import com.cloudmine.api.CMUser;
-import com.cloudmine.api.rest.callbacks.CMResponseCallback;
+import com.cloudmine.api.SimpleCMObject;
+import com.cloudmine.api.rest.callbacks.CreationResponseCallback;
 import com.cloudmine.api.rest.callbacks.LoginResponseCallback;
 import com.cloudmine.api.rest.response.CMResponse;
+import com.cloudmine.api.rest.response.CreationResponse;
 import com.cloudmine.api.rest.response.LoginResponse;
 import com.cloudmine.api.rest.response.code.CMResponseCode;
 import com.cloudmine.test.CloudMineTestRunner;
 import com.cloudmine.test.ServiceTestBase;
 import com.cloudmine.test.TestServiceCallback;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -55,20 +58,40 @@ public class CMUserIntegrationTest extends ServiceTestBase {
     }
 
     @Test
+    @Ignore //TODO we need to be able to delete users for this to work!
     public void testCreateUser() {
+        final CMUser user = CMUser.CMUser("user45435345x345f3@user.com", "w");
+
+        user.createUser(TestServiceCallback.testCallback(new CreationResponseCallback() {
+            @Override
+            public void onCompletion(CreationResponse response) {
+                assertTrue(response.wasSuccess());
+                assertEquals(response.getObjectId(), user.getObjectId());
+            }
+        }));
+        waitThenAssertTestResults();
+    }
+
+    @Test
+    public void testCreateUserErrors() {
         CMUser user = CMUser.CMUser("@.notanEm!l", "pw");
-        user.createUser(TestServiceCallback.testCallback(new CMResponseCallback() {
+        user.createUser(TestServiceCallback.testCallback(new CreationResponseCallback() {
             public void onCompletion(CMResponse response) {
                 assertEquals(CMResponseCode.INVALID_EMAIL_OR_MISSING_PASSWORD, response.getResponseCode());
             }
         }));
         waitThenAssertTestResults();
         user = CMUser.CMUser("vali@email.com", "");
-        user.createUser(TestServiceCallback.testCallback(new CMResponseCallback() {
+        user.createUser(TestServiceCallback.testCallback(new CreationResponseCallback() {
             public void onCompletion(CMResponse response) {
                 assertEquals(CMResponseCode.INVALID_EMAIL_OR_MISSING_PASSWORD, response.getResponseCode());
             }
         }));
         waitThenAssertTestResults();
+    }
+
+    @Test
+    public void testUserProfile() {
+        SimpleCMObject.SimpleCMObject().add(null, null);
     }
 }
