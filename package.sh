158938958d185653e45f92c22d2c9f6a0f9b2868
Ethan Mick <ethan@cloudmine.me>
2013-01-25 15:50:38 -0500

    Updated Package script to also package Java SDK

--- package.sh
+++ package.sh
@@ -5,7 +5,7 @@ CLOUDMINE_ANDROID_VERSION=0.5
 ANDROID_HOME=.
 JAVA_SDK_HOME=./cloudmine-javasdk
 DEPLOY_DIR="cloudmine-android-v$CLOUDMINE_ANDROID_VERSION"
-ARCHIVE_NAME="$DEPLOY_DIR.tgz"
+ARCHIVE_NAME="$DEPLOY_DIR.zip"
 
 echo "Building Java Library..."
 mvn package -DskipTests=true -f $JAVA_SDK_HOME/pom.xml;mvn install:install-file -DgroupId=com.cloudmine.api -DartifactId=cloudmine-javasdk -Dversion=$CLOUDMINE_JAVASDK_VERSION -Dpackaging=jar -DpomFile=$JAVA_SDK_HOME/pom.xml -Dfile=$JAVA_SDK_HOME/target/cloudmine-javasdk-$CLOUDMINE_JAVASDK_VERSION.jar -DgeneratePom=true
@@ -18,13 +18,13 @@ mvn assembly:assembly -DdescriptorId=jar-with-dependencies -DbuildFor=android -D
 
 echo "Building Java docs..."
 ANDROID_DOCS=android-javadocs
-DOCS_ARCHIVE="$ANDROID_DOCS-$CLOUDMINE_ANDROID_VERSION.tgz"
+DOCS_ARCHIVE="$ANDROID_DOCS-$CLOUDMINE_ANDROID_VERSION.zip"
 rm -rf $ANDROID_HOME/target/$ANDROID_DOCS
 mkdir $ANDROID_HOME/target/$ANDROID_DOCS
 
 javadoc $ANDROID_HOME/src/main/java/com.cloudmine.api/*.java $ANDROID_HOME/src/main/java/com.cloudmine.api/gui/*.java $ANDROID_HOME/src/main/java/com.cloudmine.api/loopj/*.java $ANDROID_HOME/src/main/java/com.cloudmine.api/rest/*.java $ANDROID_HOME/src/main/java/com.cloudmine.api/rest/callbacks/*.java $JAVA_SDK_HOME/src/main/java/com/cloudmine/api/*.java $JAVA_SDK_HOME/src/main/java/com/cloudmine/api/rest/*.java $JAVA_SDK_HOME/src/main/java/com/cloudmine/api/rest/callbacks/*.java $JAVA_SDK_HOME/src/main/java/com/cloudmine/api/rest/options/*.java $JAVA_SDK_HOME/src/main/java/com/cloudmine/api/rest/response/*.java $JAVA_SDK_HOME/src/main/java/com/cloudmine/api/rest/response/code/*.java $JAVA_SDK_HOME/src/main/java/com/cloudmine/api/persistance/*.java $JAVA_SDK_HOME/src/main/java/com/cloudmine/api/exceptions/*.java -d $ANDROID_HOME/target/$ANDROID_DOCS -doctitle "CloudMine Android $CLOUDMINE_ANDROID_VERSION API" -windowtitle "CloudMine Android API" -use -classpath $JAVA_SDK_HOME/target/lib/jackson-annotations-2.0.0-RC2.jar:$JAVA_SDK_HOME/target/lib/jackson-core-2.0.0-RC2.jar:$JAVA_SDK_HOME/target/lib/jackson-databind-2.0.0-RC2.jar
 cd "$ANDROID_HOME/target"
-tar -cvf "../$DOCS_ARCHIVE" $ANDROID_DOCS 
+zip -r "../$DOCS_ARCHIVE" $ANDROID_DOCS 
 cd ..
 
 echo "Copying files to deploy..."
@@ -34,11 +34,37 @@ cp $DOCS_ARCHIVE $DEPLOY_DIR
 cp "$JAVA_SDK_HOME/CHANGELOG.md" $DEPLOY_DIR
 cp "$JAVA_SDK_HOME/LICENSE" $DEPLOY_DIR
 
-echo "Tarballing dpeploy directory..."
+echo "Zipping deploy directory..."
 if [ -e $ARCHIVE_NAME ]; then
     rm $ARCHIVE_NAME
 fi
 
-tar -cvf $ARCHIVE_NAME $DEPLOY_DIR
+zip -r $ARCHIVE_NAME $DEPLOY_DIR
 rm -rf $DEPLOY_DIR
 rm $DOCS_ARCHIVE
+
+echo "Creating Java SDK deploy directory..."
+cd $JAVA_SDK_HOME
+echo "Running Maven Javadoc generation for Java SDK"
+mvn javadoc:javadoc
+
+JAVA_DOCS_ARCHIVE="javadocs-$CLOUDMINE_JAVASDK_VERSION.zip"
+DEPLOY_JAVA_DIR="cloudmine-java-v$CLOUDMINE_JAVASDK_VERSION"
+ARCHIVE_JAVA_NAME="$DEPLOY_JAVA_DIR.zip"
+cd "target/site"
+zip -r "../../$JAVA_DOCS_ARCHIVE" "apidocs"
+cd "../.."
+
+mkdir $DEPLOY_JAVA_DIR
+cp "CHANGELOG.md" $DEPLOY_JAVA_DIR
+cp "LICENSE" $DEPLOY_JAVA_DIR
+cp "target/cloudmine-javasdk-$CLOUDMINE_JAVASDK_VERSION.jar" $DEPLOY_JAVA_DIR
+cp "$JAVA_DOCS_ARCHIVE" $DEPLOY_JAVA_DIR
+
+echo "Zipping deploy directory..."
+if [ -e $ARCHIVE_JAVA_NAME ]; then
+    rm $ARCHIVE_JAVA_NAME
+fi
+
+zip -r $ARCHIVE_JAVA_NAME $DEPLOY_JAVA_DIR
+rm -rf $DEPLOY_JAVA_DIR
