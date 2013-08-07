0870baefb1e47bed9da7ed8dfc655b012789b66f
JohnMcCarthy <john@cloudmine.me>
2013-08-07 10:01:50 -0400

    Automated code changes

--- src/gen/ObjectDeleteRequest.java
+++ src/gen/ObjectDeleteRequest.java
@@ -2,70 +2,6 @@ package com.cloudmine.api.rest;
 
 public class ObjectDeleteRequest extends com.cloudmine.api.rest.BaseObjectDeleteRequest {
 
-    public ObjectDeleteRequest(boolean deleteAll) {
-        super(deleteAll, (com.cloudmine.api.CMSessionToken)null, (com.cloudmine.api.rest.options.CMServerFunction)null, (com.android.cloudmine.Response.Listener<com.cloudmine.api.rest.response.ObjectModificationResponse>)null, (com.android.cloudmine.Response.ErrorListener)null);
-    }
-
-    public ObjectDeleteRequest(boolean deleteAll, com.cloudmine.api.CMSessionToken sessionToken) {
-        super(deleteAll, sessionToken, (com.cloudmine.api.rest.options.CMServerFunction)null, (com.android.cloudmine.Response.Listener<com.cloudmine.api.rest.response.ObjectModificationResponse>)null, (com.android.cloudmine.Response.ErrorListener)null);
-    }
-
-    public ObjectDeleteRequest(boolean deleteAll, com.cloudmine.api.rest.options.CMServerFunction serverFunction) {
-        super(deleteAll, (com.cloudmine.api.CMSessionToken)null, serverFunction, (com.android.cloudmine.Response.Listener<com.cloudmine.api.rest.response.ObjectModificationResponse>)null, (com.android.cloudmine.Response.ErrorListener)null);
-    }
-
-    public ObjectDeleteRequest(boolean deleteAll, com.cloudmine.api.CMSessionToken sessionToken, com.cloudmine.api.rest.options.CMServerFunction serverFunction) {
-        super(deleteAll, sessionToken, serverFunction, (com.android.cloudmine.Response.Listener<com.cloudmine.api.rest.response.ObjectModificationResponse>)null, (com.android.cloudmine.Response.ErrorListener)null);
-    }
-
-    public ObjectDeleteRequest(boolean deleteAll, com.android.cloudmine.Response.Listener<com.cloudmine.api.rest.response.ObjectModificationResponse> responseListener) {
-        super(deleteAll, (com.cloudmine.api.CMSessionToken)null, (com.cloudmine.api.rest.options.CMServerFunction)null, responseListener, (com.android.cloudmine.Response.ErrorListener)null);
-    }
-
-    public ObjectDeleteRequest(boolean deleteAll, com.cloudmine.api.CMSessionToken sessionToken, com.android.cloudmine.Response.Listener<com.cloudmine.api.rest.response.ObjectModificationResponse> responseListener) {
-        super(deleteAll, sessionToken, (com.cloudmine.api.rest.options.CMServerFunction)null, responseListener, (com.android.cloudmine.Response.ErrorListener)null);
-    }
-
-    public ObjectDeleteRequest(boolean deleteAll, com.cloudmine.api.rest.options.CMServerFunction serverFunction, com.android.cloudmine.Response.Listener<com.cloudmine.api.rest.response.ObjectModificationResponse> responseListener) {
-        super(deleteAll, (com.cloudmine.api.CMSessionToken)null, serverFunction, responseListener, (com.android.cloudmine.Response.ErrorListener)null);
-    }
-
-    public ObjectDeleteRequest(boolean deleteAll, com.cloudmine.api.CMSessionToken sessionToken, com.cloudmine.api.rest.options.CMServerFunction serverFunction, com.android.cloudmine.Response.Listener<com.cloudmine.api.rest.response.ObjectModificationResponse> responseListener) {
-        super(deleteAll, sessionToken, serverFunction, responseListener, (com.android.cloudmine.Response.ErrorListener)null);
-    }
-
-    public ObjectDeleteRequest(boolean deleteAll, com.android.cloudmine.Response.ErrorListener errorListener) {
-        super(deleteAll, (com.cloudmine.api.CMSessionToken)null, (com.cloudmine.api.rest.options.CMServerFunction)null, (com.android.cloudmine.Response.Listener<com.cloudmine.api.rest.response.ObjectModificationResponse>)null, errorListener);
-    }
-
-    public ObjectDeleteRequest(boolean deleteAll, com.cloudmine.api.CMSessionToken sessionToken, com.android.cloudmine.Response.ErrorListener errorListener) {
-        super(deleteAll, sessionToken, (com.cloudmine.api.rest.options.CMServerFunction)null, (com.android.cloudmine.Response.Listener<com.cloudmine.api.rest.response.ObjectModificationResponse>)null, errorListener);
-    }
-
-    public ObjectDeleteRequest(boolean deleteAll, com.cloudmine.api.rest.options.CMServerFunction serverFunction, com.android.cloudmine.Response.ErrorListener errorListener) {
-        super(deleteAll, (com.cloudmine.api.CMSessionToken)null, serverFunction, (com.android.cloudmine.Response.Listener<com.cloudmine.api.rest.response.ObjectModificationResponse>)null, errorListener);
-    }
-
-    public ObjectDeleteRequest(boolean deleteAll, com.cloudmine.api.CMSessionToken sessionToken, com.cloudmine.api.rest.options.CMServerFunction serverFunction, com.android.cloudmine.Response.ErrorListener errorListener) {
-        super(deleteAll, sessionToken, serverFunction, (com.android.cloudmine.Response.Listener<com.cloudmine.api.rest.response.ObjectModificationResponse>)null, errorListener);
-    }
-
-    public ObjectDeleteRequest(boolean deleteAll, com.android.cloudmine.Response.Listener<com.cloudmine.api.rest.response.ObjectModificationResponse> responseListener, com.android.cloudmine.Response.ErrorListener errorListener) {
-        super(deleteAll, (com.cloudmine.api.CMSessionToken)null, (com.cloudmine.api.rest.options.CMServerFunction)null, responseListener, errorListener);
-    }
-
-    public ObjectDeleteRequest(boolean deleteAll, com.cloudmine.api.CMSessionToken sessionToken, com.android.cloudmine.Response.Listener<com.cloudmine.api.rest.response.ObjectModificationResponse> responseListener, com.android.cloudmine.Response.ErrorListener errorListener) {
-        super(deleteAll, sessionToken, (com.cloudmine.api.rest.options.CMServerFunction)null, responseListener, errorListener);
-    }
-
-    public ObjectDeleteRequest(boolean deleteAll, com.cloudmine.api.rest.options.CMServerFunction serverFunction, com.android.cloudmine.Response.Listener<com.cloudmine.api.rest.response.ObjectModificationResponse> responseListener, com.android.cloudmine.Response.ErrorListener errorListener) {
-        super(deleteAll, (com.cloudmine.api.CMSessionToken)null, serverFunction, responseListener, errorListener);
-    }
-
-    public ObjectDeleteRequest(boolean deleteAll, com.cloudmine.api.CMSessionToken sessionToken, com.cloudmine.api.rest.options.CMServerFunction serverFunction, com.android.cloudmine.Response.Listener<com.cloudmine.api.rest.response.ObjectModificationResponse> responseListener, com.android.cloudmine.Response.ErrorListener errorListener) {
-        super(deleteAll, sessionToken, serverFunction, responseListener, errorListener);
-    }
-
     public ObjectDeleteRequest(java.lang.String objectId) {
         super(java.util.Collections.singleton(objectId), (com.cloudmine.api.CMSessionToken)null, (com.cloudmine.api.rest.options.CMServerFunction)null, (com.android.cloudmine.Response.Listener<com.cloudmine.api.rest.response.ObjectModificationResponse>)null, (com.android.cloudmine.Response.ErrorListener)null);
     }
@@ -192,6 +128,70 @@ public class ObjectDeleteRequest extends com.cloudmine.api.rest.BaseObjectDelete
 
     public ObjectDeleteRequest(java.util.Collection<java.lang.String> objectId, com.cloudmine.api.CMSessionToken sessionToken, com.cloudmine.api.rest.options.CMServerFunction serverFunction, com.android.cloudmine.Response.Listener<com.cloudmine.api.rest.response.ObjectModificationResponse> responseListener, com.android.cloudmine.Response.ErrorListener errorListener) {
         super(objectId, sessionToken, serverFunction, responseListener, errorListener);
+    }
+
+    public ObjectDeleteRequest(boolean deleteAll) {
+        super(deleteAll, (com.cloudmine.api.CMSessionToken)null, (com.cloudmine.api.rest.options.CMServerFunction)null, (com.android.cloudmine.Response.Listener<com.cloudmine.api.rest.response.ObjectModificationResponse>)null, (com.android.cloudmine.Response.ErrorListener)null);
+    }
+
+    public ObjectDeleteRequest(boolean deleteAll, com.cloudmine.api.CMSessionToken sessionToken) {
+        super(deleteAll, sessionToken, (com.cloudmine.api.rest.options.CMServerFunction)null, (com.android.cloudmine.Response.Listener<com.cloudmine.api.rest.response.ObjectModificationResponse>)null, (com.android.cloudmine.Response.ErrorListener)null);
+    }
+
+    public ObjectDeleteRequest(boolean deleteAll, com.cloudmine.api.rest.options.CMServerFunction serverFunction) {
+        super(deleteAll, (com.cloudmine.api.CMSessionToken)null, serverFunction, (com.android.cloudmine.Response.Listener<com.cloudmine.api.rest.response.ObjectModificationResponse>)null, (com.android.cloudmine.Response.ErrorListener)null);
+    }
+
+    public ObjectDeleteRequest(boolean deleteAll, com.cloudmine.api.CMSessionToken sessionToken, com.cloudmine.api.rest.options.CMServerFunction serverFunction) {
+        super(deleteAll, sessionToken, serverFunction, (com.android.cloudmine.Response.Listener<com.cloudmine.api.rest.response.ObjectModificationResponse>)null, (com.android.cloudmine.Response.ErrorListener)null);
+    }
+
+    public ObjectDeleteRequest(boolean deleteAll, com.android.cloudmine.Response.Listener<com.cloudmine.api.rest.response.ObjectModificationResponse> responseListener) {
+        super(deleteAll, (com.cloudmine.api.CMSessionToken)null, (com.cloudmine.api.rest.options.CMServerFunction)null, responseListener, (com.android.cloudmine.Response.ErrorListener)null);
+    }
+
+    public ObjectDeleteRequest(boolean deleteAll, com.cloudmine.api.CMSessionToken sessionToken, com.android.cloudmine.Response.Listener<com.cloudmine.api.rest.response.ObjectModificationResponse> responseListener) {
+        super(deleteAll, sessionToken, (com.cloudmine.api.rest.options.CMServerFunction)null, responseListener, (com.android.cloudmine.Response.ErrorListener)null);
+    }
+
+    public ObjectDeleteRequest(boolean deleteAll, com.cloudmine.api.rest.options.CMServerFunction serverFunction, com.android.cloudmine.Response.Listener<com.cloudmine.api.rest.response.ObjectModificationResponse> responseListener) {
+        super(deleteAll, (com.cloudmine.api.CMSessionToken)null, serverFunction, responseListener, (com.android.cloudmine.Response.ErrorListener)null);
+    }
+
+    public ObjectDeleteRequest(boolean deleteAll, com.cloudmine.api.CMSessionToken sessionToken, com.cloudmine.api.rest.options.CMServerFunction serverFunction, com.android.cloudmine.Response.Listener<com.cloudmine.api.rest.response.ObjectModificationResponse> responseListener) {
+        super(deleteAll, sessionToken, serverFunction, responseListener, (com.android.cloudmine.Response.ErrorListener)null);
+    }
+
+    public ObjectDeleteRequest(boolean deleteAll, com.android.cloudmine.Response.ErrorListener errorListener) {
+        super(deleteAll, (com.cloudmine.api.CMSessionToken)null, (com.cloudmine.api.rest.options.CMServerFunction)null, (com.android.cloudmine.Response.Listener<com.cloudmine.api.rest.response.ObjectModificationResponse>)null, errorListener);
+    }
+
+    public ObjectDeleteRequest(boolean deleteAll, com.cloudmine.api.CMSessionToken sessionToken, com.android.cloudmine.Response.ErrorListener errorListener) {
+        super(deleteAll, sessionToken, (com.cloudmine.api.rest.options.CMServerFunction)null, (com.android.cloudmine.Response.Listener<com.cloudmine.api.rest.response.ObjectModificationResponse>)null, errorListener);
+    }
+
+    public ObjectDeleteRequest(boolean deleteAll, com.cloudmine.api.rest.options.CMServerFunction serverFunction, com.android.cloudmine.Response.ErrorListener errorListener) {
+        super(deleteAll, (com.cloudmine.api.CMSessionToken)null, serverFunction, (com.android.cloudmine.Response.Listener<com.cloudmine.api.rest.response.ObjectModificationResponse>)null, errorListener);
+    }
+
+    public ObjectDeleteRequest(boolean deleteAll, com.cloudmine.api.CMSessionToken sessionToken, com.cloudmine.api.rest.options.CMServerFunction serverFunction, com.android.cloudmine.Response.ErrorListener errorListener) {
+        super(deleteAll, sessionToken, serverFunction, (com.android.cloudmine.Response.Listener<com.cloudmine.api.rest.response.ObjectModificationResponse>)null, errorListener);
+    }
+
+    public ObjectDeleteRequest(boolean deleteAll, com.android.cloudmine.Response.Listener<com.cloudmine.api.rest.response.ObjectModificationResponse> responseListener, com.android.cloudmine.Response.ErrorListener errorListener) {
+        super(deleteAll, (com.cloudmine.api.CMSessionToken)null, (com.cloudmine.api.rest.options.CMServerFunction)null, responseListener, errorListener);
+    }
+
+    public ObjectDeleteRequest(boolean deleteAll, com.cloudmine.api.CMSessionToken sessionToken, com.android.cloudmine.Response.Listener<com.cloudmine.api.rest.response.ObjectModificationResponse> responseListener, com.android.cloudmine.Response.ErrorListener errorListener) {
+        super(deleteAll, sessionToken, (com.cloudmine.api.rest.options.CMServerFunction)null, responseListener, errorListener);
+    }
+
+    public ObjectDeleteRequest(boolean deleteAll, com.cloudmine.api.rest.options.CMServerFunction serverFunction, com.android.cloudmine.Response.Listener<com.cloudmine.api.rest.response.ObjectModificationResponse> responseListener, com.android.cloudmine.Response.ErrorListener errorListener) {
+        super(deleteAll, (com.cloudmine.api.CMSessionToken)null, serverFunction, responseListener, errorListener);
+    }
+
+    public ObjectDeleteRequest(boolean deleteAll, com.cloudmine.api.CMSessionToken sessionToken, com.cloudmine.api.rest.options.CMServerFunction serverFunction, com.android.cloudmine.Response.Listener<com.cloudmine.api.rest.response.ObjectModificationResponse> responseListener, com.android.cloudmine.Response.ErrorListener errorListener) {
+        super(deleteAll, sessionToken, serverFunction, responseListener, errorListener);
     }
 
 }
--- src/gen/ObjectModificationRequest.java
+++ src/gen/ObjectModificationRequest.java
@@ -2,6 +2,22 @@ package com.cloudmine.api.rest;
 
 public class ObjectModificationRequest extends com.cloudmine.api.rest.BaseObjectModificationRequest {
 
+    public ObjectModificationRequest(com.cloudmine.api.rest.Transportable savable, com.cloudmine.api.rest.callbacks.Callback<com.cloudmine.api.rest.response.ObjectModificationResponse> callback) {
+        super(savable, (com.cloudmine.api.CMSessionToken)null, (com.cloudmine.api.rest.options.CMServerFunction)null, callback);
+    }
+
+    public ObjectModificationRequest(com.cloudmine.api.rest.Transportable savable, com.cloudmine.api.CMSessionToken userSession, com.cloudmine.api.rest.callbacks.Callback<com.cloudmine.api.rest.response.ObjectModificationResponse> callback) {
+        super(savable, userSession, (com.cloudmine.api.rest.options.CMServerFunction)null, callback);
+    }
+
+    public ObjectModificationRequest(com.cloudmine.api.rest.Transportable savable, com.cloudmine.api.rest.options.CMServerFunction serverFunction, com.cloudmine.api.rest.callbacks.Callback<com.cloudmine.api.rest.response.ObjectModificationResponse> callback) {
+        super(savable, (com.cloudmine.api.CMSessionToken)null, serverFunction, callback);
+    }
+
+    public ObjectModificationRequest(com.cloudmine.api.rest.Transportable savable, com.cloudmine.api.CMSessionToken userSession, com.cloudmine.api.rest.options.CMServerFunction serverFunction, com.cloudmine.api.rest.callbacks.Callback<com.cloudmine.api.rest.response.ObjectModificationResponse> callback) {
+        super(savable, userSession, serverFunction, callback);
+    }
+
     public ObjectModificationRequest(com.cloudmine.api.rest.Transportable savable) {
         super(savable, (com.cloudmine.api.CMSessionToken)null, (com.cloudmine.api.rest.options.CMServerFunction)null, (com.android.cloudmine.Response.Listener<com.cloudmine.api.rest.response.ObjectModificationResponse>)null, (com.android.cloudmine.Response.ErrorListener)null);
     }
@@ -64,22 +80,6 @@ public class ObjectModificationRequest extends com.cloudmine.api.rest.BaseObject
 
     public ObjectModificationRequest(com.cloudmine.api.rest.Transportable savable, com.cloudmine.api.CMSessionToken token, com.cloudmine.api.rest.options.CMServerFunction serverFunction, com.android.cloudmine.Response.Listener<com.cloudmine.api.rest.response.ObjectModificationResponse> successListener, com.android.cloudmine.Response.ErrorListener errorListener) {
         super(savable, token, serverFunction, successListener, errorListener);
-    }
-
-    public ObjectModificationRequest(com.cloudmine.api.rest.Transportable savable, com.cloudmine.api.rest.callbacks.Callback<com.cloudmine.api.rest.response.ObjectModificationResponse> callback) {
-        super(savable, (com.cloudmine.api.CMSessionToken)null, (com.cloudmine.api.rest.options.CMServerFunction)null, callback);
-    }
-
-    public ObjectModificationRequest(com.cloudmine.api.rest.Transportable savable, com.cloudmine.api.CMSessionToken userSession, com.cloudmine.api.rest.callbacks.Callback<com.cloudmine.api.rest.response.ObjectModificationResponse> callback) {
-        super(savable, userSession, (com.cloudmine.api.rest.options.CMServerFunction)null, callback);
-    }
-
-    public ObjectModificationRequest(com.cloudmine.api.rest.Transportable savable, com.cloudmine.api.rest.options.CMServerFunction serverFunction, com.cloudmine.api.rest.callbacks.Callback<com.cloudmine.api.rest.response.ObjectModificationResponse> callback) {
-        super(savable, (com.cloudmine.api.CMSessionToken)null, serverFunction, callback);
-    }
-
-    public ObjectModificationRequest(com.cloudmine.api.rest.Transportable savable, com.cloudmine.api.CMSessionToken userSession, com.cloudmine.api.rest.options.CMServerFunction serverFunction, com.cloudmine.api.rest.callbacks.Callback<com.cloudmine.api.rest.response.ObjectModificationResponse> callback) {
-        super(savable, userSession, serverFunction, callback);
     }
 
 }
