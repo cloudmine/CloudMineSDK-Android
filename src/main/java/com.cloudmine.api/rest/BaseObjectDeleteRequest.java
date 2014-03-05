619c718a27a6195d85a01e62b2a83a26617a3b4a
JohnMcCarthy <john@cloudmine.me>
2014-03-05 13:47:51 -0500

    Comment delete object request

--- src/main/java/com.cloudmine.api/rest/BaseObjectDeleteRequest.java
+++ src/main/java/com.cloudmine.api/rest/BaseObjectDeleteRequest.java
@@ -12,6 +12,7 @@ import me.cloudmine.annotations.Single;
 import java.util.Collection;
 
 /**
+ * A request for deleting one or more cloudmine objects
  * <br>
  * Copyright CloudMine LLC. All rights reserved<br>
  * See LICENSE file included with SDK for details.
@@ -20,11 +21,28 @@ public class BaseObjectDeleteRequest extends CloudMineRequest<ObjectModification
     public static final int REQUEST_TYPE = 408;
     private static final CMURLBuilder BASE_URL = new CMURLBuilder("/data", true);
 
+    /**
+     * Delete all of the objects with the given ids
+     * @param objectIds the ids of the objects to delete
+     * @param sessionToken an optional sessionToken. If specified, it is assumed the objects are user level
+     * @param serverFunction
+     * @param responseListener
+     * @param errorListener
+     */
     @Expand
     public BaseObjectDeleteRequest(@Single Collection<String> objectIds, @Optional CMSessionToken sessionToken, @Optional CMServerFunction serverFunction, @Optional Response.Listener<ObjectModificationResponse> responseListener, @Optional Response.ErrorListener errorListener) {
         super(Method.DELETE, new CMURLBuilder("", true).delete(objectIds).user(sessionToken), sessionToken, serverFunction, responseListener, errorListener);
     }
 
+    /**
+     * Create a new ObjectDeleteRequest for deleting all objects. This request will do nothing
+     * if deleteAll is equal to false
+     * @param deleteAll True to delete all objects; if false, the request has no effect
+     * @param sessionToken An optional session token. If specified, deletes all user level data for the user who is logged in
+     * @param serverFunction
+     * @param responseListener
+     * @param errorListener
+     */
     @Expand
     public BaseObjectDeleteRequest(boolean deleteAll, @Optional CMSessionToken sessionToken, @Optional CMServerFunction serverFunction, @Optional Response.Listener<ObjectModificationResponse> responseListener, @Optional Response.ErrorListener errorListener) {
         super(Method.DELETE,
