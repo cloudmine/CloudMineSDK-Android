e3f6398c48b845b04d4ca8bc3d497b1485a12bcb
JohnMcCarthy <john@cloudmine.me>
2014-03-05 11:39:35 -0500

    Comment ACLCreateRequest

--- src/main/java/com.cloudmine.api/rest/BaseAccessListCreateRequest.java
+++ src/main/java/com.cloudmine.api/rest/BaseAccessListCreateRequest.java
@@ -10,12 +10,22 @@ import me.cloudmine.annotations.Expand;
 import me.cloudmine.annotations.Optional;
 
 /**
+ * Create or update an AccessList. Requires a valid CMSessionToken
  * <br>Copyright CloudMine LLC. All rights reserved
  * <br> See LICENSE file included with SDK for details.
  */
 public class BaseAccessListCreateRequest extends CloudMineRequest<CreationResponse> {
     public static final int REQUEST_TYPE = 420;
     private static final String BASE_URL = "/user/access";
+
+    /**
+     * Create a new AccessListCreateRequest for a specific access list
+     * @param accessList The access list to create
+     * @param sessionToken a valid session token for the user associated with this access list
+     * @param serverFunction
+     * @param successListener
+     * @param errorListener
+     */
     @Expand
     public BaseAccessListCreateRequest(CMAccessList accessList, CMSessionToken sessionToken, @Optional CMServerFunction serverFunction, @Optional Response.Listener<CreationResponse> successListener, @Optional Response.ErrorListener errorListener) {
         super(Method.POST, BASE_URL, accessList.transportableRepresentation(), sessionToken, serverFunction, successListener, errorListener);
