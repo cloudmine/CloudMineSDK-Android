5987e31a2761910190565a445c8a9ff5c9a0fa16
JohnMcCarthy <john@cloudmine.me>
2014-03-05 12:53:16 -0500

    Comment changeuserpasswordrequest

--- src/main/java/com.cloudmine.api/rest/BaseChangeUserPasswordRequest.java
+++ src/main/java/com.cloudmine.api/rest/BaseChangeUserPasswordRequest.java
@@ -6,12 +6,15 @@ import com.android.volley.Response;
 import com.cloudmine.api.LibrarySpecificClassCreator;
 import com.cloudmine.api.rest.options.CMServerFunction;
 import com.cloudmine.api.rest.response.CMResponse;
+import me.cloudmine.annotations.Expand;
 import me.cloudmine.annotations.Optional;
 
 import java.util.Map;
 
 /**
- *
+ * A Request for changing a user's password.
+ * <br>Copyright CloudMine LLC. All rights reserved
+ * <br> See LICENSE file included with SDK for details.
  */
 public class BaseChangeUserPasswordRequest extends CloudMineRequest<CMResponse> {
 
@@ -20,6 +23,16 @@ public class BaseChangeUserPasswordRequest extends CloudMineRequest<CMResponse> 
     private final String emailOrUsername;
     private final String oldPassword;
 
+    /**
+     * Create a new BaseChangeUserPasswordRequest
+     * @param emailOrUsername the username or password the user uses to log in
+     * @param oldPassword the user's current password
+     * @param newPassword the user's desired password
+     * @param serverFunction
+     * @param successListener
+     * @param errorListener
+     */
+    @Expand
     public BaseChangeUserPasswordRequest(String emailOrUsername, String oldPassword, String newPassword, @Optional CMServerFunction serverFunction, @Optional Response.Listener<CMResponse> successListener, @Optional Response.ErrorListener errorListener) {
         super(Method.POST, "/account/password/change", "{\"password\":\"" + newPassword + "\"}", null, serverFunction, successListener, errorListener);
         this.emailOrUsername = emailOrUsername;
