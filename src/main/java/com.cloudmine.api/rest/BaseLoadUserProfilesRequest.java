09965defe72871c1543e02e2a24d578bc51f1581
JohnMcCarthy <john@cloudmine.me>
2014-03-05 13:39:02 -0500

    Comment LoadUserProfilesRequest

--- src/main/java/com.cloudmine.api/rest/BaseLoadUserProfilesRequest.java
+++ src/main/java/com.cloudmine.api/rest/BaseLoadUserProfilesRequest.java
@@ -8,17 +8,32 @@ import me.cloudmine.annotations.Expand;
 import me.cloudmine.annotations.Optional;
 
 /**
- *
+ * A Request for loading user profiles
+ * <br>Copyright CloudMine LLC. All rights reserved
+ * <br> See LICENSE file included with SDK for details.
  */
 public class BaseLoadUserProfilesRequest extends CloudMineRequest<CMObjectResponse> {
 
     public static final int REQUEST_TYPE = 410;
 
+    /**
+     * Create a new Request for loading all user profiles
+     * @param serverFunction
+     * @param successListener
+     * @param errorListener
+     */
     @Expand
     public BaseLoadUserProfilesRequest(@Optional CMServerFunction serverFunction, @Optional Response.Listener<CMObjectResponse> successListener, @Optional Response.ErrorListener errorListener){
         super(Method.GET, "/account", null, null, serverFunction, successListener, errorListener);
     }
 
+    /**
+     * Create a new request for loading all user profiles that match the specified search string
+     * @param searchString A search string. It is recommend that {@link com.cloudmine.api.SearchQuery} is used to construct this string
+     * @param serverFunction
+     * @param successListener
+     * @param errorListener
+     */
     @Expand
     public BaseLoadUserProfilesRequest(String searchString, @Optional CMServerFunction serverFunction, @Optional Response.Listener<CMObjectResponse> successListener, @Optional Response.ErrorListener errorListener){
         super(Method.GET, "/account/search?p=" + CMURLBuilder.encode(searchString), null, null, serverFunction, successListener, errorListener);
