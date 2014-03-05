6f21aae5d5ffc2f4b02549ab32f5cd4f24d0ff21
JohnMcCarthy <john@cloudmine.me>
2014-03-05 12:39:27 -0500

    Comment ChangeUserIdentifierRequest

--- src/main/java/com.cloudmine.api/rest/BaseChangeUserIdentifierRequest.java
+++ src/main/java/com.cloudmine.api/rest/BaseChangeUserIdentifierRequest.java
@@ -13,20 +13,43 @@ import me.cloudmine.annotations.Optional;
 import java.util.Map;
 
 /**
- *
+ * A Request for changing a user's identifier - their e-mail or username that they use to
+ * log in. Changing a user identifier invalidates all old sessions.
+ * <br>Copyright CloudMine LLC. All rights reserved
+ * <br> See LICENSE file included with SDK for details.
  */
 public class BaseChangeUserIdentifierRequest extends CloudMineRequest<CMResponse> {
 
     public static final int REQUEST_TYPE = 412;
 
+    /**
+     * Create a new BaseChangeUserIdentifierRequest for changing a user's e-mail address
+     * @param currentEmail
+     * @param password
+     * @param newEmail
+     * @param serverFunction
+     * @param successListener
+     * @param errorListener
+     * @return
+     */
     @Expand(isStatic = true)
-    public static CloudMineRequest changeUserEmail(String userIdentifier, String password, String newEmail, @Optional CMServerFunction serverFunction, @Optional Response.Listener<CMResponse> successListener, @Optional Response.ErrorListener errorListener){
-        return new BaseChangeUserIdentifierRequest(userIdentifier, password, newEmail, null, serverFunction, successListener, errorListener);
+    public static CloudMineRequest changeUserEmail(String currentEmail, String password, String newEmail, @Optional CMServerFunction serverFunction, @Optional Response.Listener<CMResponse> successListener, @Optional Response.ErrorListener errorListener){
+        return new BaseChangeUserIdentifierRequest(currentEmail, password, newEmail, null, serverFunction, successListener, errorListener);
     }
 
+    /**
+     * Create a new BaseChangeUserIdentifierRequest for changing a user's username
+     * @param userName
+     * @param password
+     * @param newUserName
+     * @param serverFunction
+     * @param successListener
+     * @param errorListener
+     * @return
+     */
     @Expand(isStatic = true)
-    public static CloudMineRequest changeUserName(String userIdentifier, String password, String newUserName, @Optional CMServerFunction serverFunction, @Optional Response.Listener<CMResponse> successListener, @Optional Response.ErrorListener errorListener){
-        return new BaseChangeUserIdentifierRequest(userIdentifier, password, null, newUserName, serverFunction, successListener, errorListener);
+    public static CloudMineRequest changeUserName(String userName, String password, String newUserName, @Optional CMServerFunction serverFunction, @Optional Response.Listener<CMResponse> successListener, @Optional Response.ErrorListener errorListener){
+        return new BaseChangeUserIdentifierRequest(userName, password, null, newUserName, serverFunction, successListener, errorListener);
     }
 
     private static String getJsonBody(String newEmail, String newUserName) {
@@ -45,6 +68,16 @@ public class BaseChangeUserIdentifierRequest extends CloudMineRequest<CMResponse
     private final String userIdentifier;
     private final String password;
 
+    /**
+     * Create a new BaseChangeUserIdentifierRequest for updating a user's email or username
+     * @param userIdentifier the user's current username or e-mail
+     * @param password the user's current password
+     * @param newEmail the new e-mail address. May be null
+     * @param newUserName the new username. May be null
+     * @param serverFunction
+     * @param successListener
+     * @param errorListener
+     */
     @Expand
     public BaseChangeUserIdentifierRequest(String userIdentifier, String password, String newEmail, String newUserName, @Optional CMServerFunction serverFunction, @Optional Response.Listener<CMResponse> successListener, @Optional Response.ErrorListener errorListener) {
         super(Method.POST, "/account/credentials", getJsonBody(newEmail, newUserName), null, serverFunction, successListener, errorListener);
